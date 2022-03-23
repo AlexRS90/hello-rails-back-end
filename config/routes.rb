2bda5474b1e009011c7ac5d547983317b0197ed7
AlexRS90 <aramosbengy@gmail.com>
2022-03-22 22:10:51 -0600

    Add routes to the API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'regards/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :regards
+    end
+  end
 end
